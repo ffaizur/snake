6ab37519391951b54aa0902f6133c0b34dfd914e
Faizur Rahman <140473957+ffaizur@users.noreply.github.com>
2025-02-02 21:31:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert('To win between 80 rupees to 100 rupees, complete up to level 51+ in this game... Press OK to start the game.');
+alert('Clear Level 51+ to win ₹50! Tap 'OK' to start your challenge!');
 
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
